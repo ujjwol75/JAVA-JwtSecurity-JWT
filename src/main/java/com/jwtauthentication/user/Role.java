1d3c6260cae91deaacd019c32a0f84b7916c1a22
ujjwol75 <ujjwolneupane75@gmail.com>
2023-05-24 08:46:53 +0545

    jwt security

---
+++ src/main/java/com/jwtauthentication/user/Role.java
@@ -0,0 +1,8 @@
+package com.jwtauthentication.user;
+
+public enum Role {
+
+	USER,
+	ADMIN
+	
+}
---
+++ src/main/java/com/jwtauthentication/user/User.java
@@ -0,0 +1,119 @@
+package com.jwtauthentication.user;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails{
+	
+	
+	@Id
+	@GeneratedValue 
+	private Integer id;
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String password;
+	
+	
+//	public Integer getId() {
+//		return id;
+//	}
+//	public void setId(Integer id) {
+//		this.id = id;
+//	}
+//	public String getFirstname() {
+//		return firstname;
+//	}
+//	public void setFirstname(String firstname) {
+//		this.firstname = firstname;
+//	}
+//	public String getLastname() {
+//		return lastname;
+//	}
+//	public void setLastname(String lastname) {
+//		this.lastname = lastname;
+//	}
+//	public String getEmail() {
+//		return email;
+//	}
+//	public void setEmail(String email) {
+//		this.email = email;
+//	}
+//	public Role getRole() {
+//		return role;
+//	}
+//	public void setRole(Role role) {
+//		this.role = role;
+//	}
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+	//@Enumerated is used to tell this object is type enum
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	
+	
+	
+	
+}
